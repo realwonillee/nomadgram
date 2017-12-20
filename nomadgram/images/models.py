1372ca0ebf5b23e38334dc8e0671fc697b862106
Delkey <wononezero@gmail.com>
2017-12-20 21:02:33 +0900

    Image admin panel

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from nomadgram.users import models as user_model
 
+
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,12 +12,41 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
+
+@python_2_unicode_compatible
 class Image(TimeStampedModel):
+
+    """ Image Model """
 
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_model.User, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_model.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return self.message
+
+
+@python_2_unicode_compatible
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_model.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
